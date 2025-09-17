04e0f00a0df04274db8eb44afb13da581127b736
hazelsilver8740 <daisyliu@Daisys-MacBook-Air.local>
2025-09-16 20:49:39 -0400

    Moved combine_data and preprocess_combined_data to preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,15 +1,57 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.impute import IterativeImputer
+from sklearn.model_selection import train_test_split
+
 from .utils import *
 
-def count_nas(df):
+def impute_data(X_train, X_test, imputer=None):
+    if imputer is None:
+        imputer = IterativeImputer(max_iter=10, random_state=42)
+    X_train_imputed = imputer.fit_transform(X_train)
+    X_test_imputed = imputer.transform(X_test)
 
-  tot = len(df)
-  cols = list(df.columns)
-  nas = [df[col].isna().sum() for col in cols]
-  percent_nas = [na/tot * 100 for na in nas]
+    return pd.DataFrame(X_train_imputed, columns=X_train.columns), pd.DataFrame(X_test_imputed, columns=X_test.columns)
 
-  na_df = pd.DataFrame.from_dict({"col": cols, "nas": nas, "percent_nas": percent_nas})
-  na_df.sort_values(by = ["percent_nas"], ascending = False, inplace = True)
-  na_df = na_df.reset_index(drop = True)
+def scale_data(X_train, X_test, scaler=None):
+    if scaler is None:
+        scaler = MinMaxScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return pd.DataFrame(X_train_scaled, columns=X_train.columns), pd.DataFrame(X_test_scaled, columns=X_test.columns)
 
-  return na_df
+def combine_data(preprocessed_df: pd.DataFrame, unstructured_df: pd.DataFrame, on = 'Patient_ID', how='inner') -> pd.DataFrame:
+    """
+    Pipeline for combining preprocessed dataframe with structured data with dataframe of embeddings.
+
+    Both dfs should have a 'Patient_ID' column on which to combine, otherwise need to specify
+    """
+    combined_df = pd.merge(preprocessed_df, unstructured_df, on = on, how = how)
+    
+    return combined_df
+
+def preprocess_combined_data(combined_df) -> dict:
+    """
+
+    """
+    cleaned_df = clean_column_names(combined_df)
+    X = cleaned_df.drop(columns = ['HasMHD'])
+    y = cleaned_df['HasMHD']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+    X_train_imputed, X_test_imputed = impute_data(X_train, X_test)
+
+    X_train_final, X_test_final = scale_data(X_train_imputed, X_test_imputed)
+
+    preprocessed_combined_data = {
+        'df': combined_df,
+        'X': X,
+        'y': y,
+        'X_train_scaled': X_train_final,
+        'X_test_scaled': X_test_final,
+        'y_train': y_train,
+        'y_test': y_test
+        }
+
+    return preprocessed_combined_data
