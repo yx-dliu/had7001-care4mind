c1dc68ecdc65ee9104486aa7a87bb40fc424f567
hazelsilver8740 <daisyliu@Daisys-MacBook-Air.local>
2025-09-16 20:47:47 -0400

    Added clean_column_names to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import re
+
+def count_nas(df):
+
+  tot = len(df)
+  cols = list(df.columns)
+  nas = [df[col].isna().sum() for col in cols]
+  percent_nas = [na/tot * 100 for na in nas]
+
+  na_df = pd.DataFrame.from_dict({"col": cols, "nas": nas, "percent_nas": percent_nas})
+  na_df.sort_values(by = ["percent_nas"], ascending = False, inplace = True)
+  na_df = na_df.reset_index(drop = True)
+
+  return na_df
 
 def flatten_dict(dict: dict) -> list:
     """
@@ -24,3 +38,7 @@ def check_Bin(codes, icd_set):
         return 0
     else:
         return int(codes in icd_set)
+    
+def clean_column_names(df):
+    df.columns = [re.sub(r'[\"\'\[\]\{\}\\,()]+', '_', col) for col in df.columns]
+    return df
