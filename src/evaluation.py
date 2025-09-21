142407703ed9eb9118ef1060bb0d9d9514b13e4b
hazelsilver8740 <daisyliu@Daisys-MacBook-Air.local>
2025-09-21 17:06:23 -0400

    Added code to save plots of confusion matrices and roc curcurves

--- src/evaluation.py
+++ src/evaluation.py
@@ -43,7 +43,7 @@ def print_scores(conf_matrix, class_report):
   print("\nClassification Report:")
   print(class_report)
 
-def plot_confusion_matrix(conf_matrix):
+def plot_confusion_matrix(conf_matrix, save_res = False, save_path = 'confusion_matrix.png'):
   """
     Plots a heatmap of the confusion matrix using Seaborn.
 
@@ -56,7 +56,12 @@ def plot_confusion_matrix(conf_matrix):
   sns.heatmap(conf_matrix, annot=True, fmt='d')
   plt.ylabel('Actual')
   plt.xlabel('Predicted')
+
+  if save_res:
+    plt.savefig(save_path, bbox_inches='tight')
+
   plt.show()
+  plt.close()
 
 def get_auc_values(y_prob, y):
   """
@@ -66,7 +71,7 @@ def get_auc_values(y_prob, y):
   roc_auc = roc_auc_score(y, y_prob)
   return fpr, tpr, roc_auc
 
-def plot_roc_curve(fpr, tpr, roc_auc, title = "ROC AUC Curve"):
+def plot_roc_curve(fpr, tpr, roc_auc, title = "ROC AUC Curve", save_res = False, save_path = 'roc_curve.png'):
   """
     Computes the false positive rate, true positive rate, and AUC score for a binary classifier.
 
@@ -89,9 +94,14 @@ def plot_roc_curve(fpr, tpr, roc_auc, title = "ROC AUC Curve"):
   plt.ylabel('True Positive Rate (TPR)')
   plt.title(title)
   plt.legend(loc='lower right')
-  plt.show()
 
-def evaluate_model(model, X_train, y_train, X_test, y_test):
+  if save_res:
+    plt.savefig(save_path, bbox_inches='tight')
+
+  plt.show()
+  plt.close()
+
+def evaluate_model(model, X_train, y_train, X_test, y_test, save_res = False, plot_names = None):
   """
     Evaluates a binary classification model using training and test data.
 
@@ -129,19 +139,47 @@ def evaluate_model(model, X_train, y_train, X_test, y_test):
   # Print scores and evaluation metrics for the training set
   print("======== Training Set ==========")
   print_scores(conf_matrix_train, class_report_train)
-  plot_confusion_matrix(conf_matrix_train) # plot the confusion matrix for the training set using the function we created above
+
+  if save_res and plot_names is not None:
+    save_path = plot_names['confusion_matrix'] + "_train.png"
+    plot_confusion_matrix(conf_matrix_train, save_res, save_path) # plot the confusion matrix for the training set using the function we created above
+  elif save_res:
+    plot_confusion_matrix(conf_matrix_train, save_res)
+  else:
+    plot_confusion_matrix(conf_matrix_train)
 
   fpr_train, tpr_train, roc_auc_train = get_auc_values(y_prob_train, y_train)
   print(f"Training AUC: {roc_auc_train:.4f}")
-  plot_roc_curve(fpr_train, tpr_train, roc_auc_train)
+  plot_roc_curve(fpr_train, tpr_train, roc_auc_train, save_res, save_path)
+
+  if save_res and plot_names is not None:
+    save_path = plot_names['roc_curve'] + "_train.png"
+    plot_roc_curve(fpr_train, tpr_train, roc_auc_train, save_res, save_path)
+  elif save_res:
+    plot_roc_curve(fpr_train, tpr_train, roc_auc_train, save_res)
+  else:
+    plot_roc_curve(fpr_train, tpr_train, roc_auc_train)
 
   # Print scores and evaluation metrics for the test set
   print('======== Test Set ==========')
   print_scores(conf_matrix_test, class_report_test)
-  plot_confusion_matrix(conf_matrix_test) # plot the confusion matrix for the test set using the function we created above
+  if save_res and plot_names is not None:
+    save_path = plot_names['confusion_matrix'] + "_test.png"
+    plot_confusion_matrix(conf_matrix_test, save_res, save_path) # plot the confusion matrix for the training set using the function we created above
+  elif save_res:
+    plot_confusion_matrix(conf_matrix_test, save_res)
+  else:
+    plot_confusion_matrix(conf_matrix_test)
 
   fpr_test, tpr_test, roc_auc_test = get_auc_values(y_prob_test, y_test)
   print(f"Testing AUC: {roc_auc_test:.4f}")
-  plot_roc_curve(fpr_test, tpr_test, roc_auc_test)
+  
+  if save_res and plot_names is not None:
+    save_path = plot_names['roc_curve'] + "_test.png"
+    plot_roc_curve(fpr_test, tpr_test, roc_auc_test, save_res, save_path)
+  elif save_res:
+    plot_roc_curve(fpr_test, tpr_test, roc_auc_test, save_res)
+  else:
+    plot_roc_curve(fpr_test, tpr_test, roc_auc_test)
 
   return y_pred_train, y_pred_test, y_prob_train, y_prob_test
