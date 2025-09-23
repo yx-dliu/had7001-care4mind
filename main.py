090ebff4d20aa520b69a035034a6d9d8be8c3984
hazelsilver8740 <daisyliu@Daisys-MacBook-Air.local>
2025-09-23 19:24:27 -0400

    Update main.py to reflect refactored src modules

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import yaml
+import json
 
-from src.pipeline import preprocess_structured_data, preprocess_and_combine_data
+from src.pipeline import (
+    preprocess_structured_data, 
+    combine_preprocessed_data,
+    load_models,
+    run_stratified_k_fold_cv)
 
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
@@ -13,20 +18,45 @@ df = pd.read_parquet(structured_path)
 # Preprocessing structured data
 df = preprocess_structured_data(df)
 df = df[config['features']]
+print(list(df.columns))
 
 # Combine and preprocess structured and unstructured data
-preprocessed_combined_data = preprocess_and_combine_data(df, config['embedding_sizes'])
+combined_preprocessed_data = combine_preprocessed_data(df, config['embedding_sizes']) # dict
 
-# Hyperparameter tuning
+# Stratified k-fold cross-validation and evaluation
+models_dict = load_models()
+results_dict = {}
 
+for embedding_size, embedding_size_data in combined_preprocessed_data.items():
+    """
+    embedding_size follows the format pca_{embedding size}
+    
+    embedding_size_data is a Pandas df with the associated training data
 
-# Stratified k-fold cross-validation
+    STILL HAVE NOT ADDED CODE FOR RUNNING LGBM
+    """
+    for model_name, model in models_dict.items():
+        print(f"Running stratified k-fold CV for {model_name}")
+        
+        # run_stratified_k_fold_cv optionally returns processed training and test
+        # data for each fold
+        model_scores, _ = run_stratified_k_fold_cv(
+            model = model, 
+            training_data = embedding_size_data, 
+            skf_n_splits = config['skf_n_splits'],
+            seed = config['seed'], 
+            label_col = config['label_col_name'], 
+            id_col = config['id_col_name'],
+            impute_max_iter = config['impute_max_iter']
+        )
 
+        results_dict[model_name + "_" + embedding_size] = model_scores
 
-# Model training and evaluation
-
+        with open(f"{model_name}_{embedding_size}_results.json", "w") as f:
+            json.dump(results_dict, f, indent = 4)
 
 # Statistical analyses
+
 
 
 if __name__ == '__main__':
